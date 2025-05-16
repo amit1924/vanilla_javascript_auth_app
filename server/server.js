1b44566164287ee9d4f9a81df811666ce0797cc1
amit1924 <amit192400@gmail.com>
2025-05-16 22:48:40 +0530

    made image ai ui responsive

--- server/server.js
+++ server/server.js
@@ -88,5 +88,5 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on ${PORT}`);
+  console.log(`🚀 Server is running on this ${PORT}`);
 });
